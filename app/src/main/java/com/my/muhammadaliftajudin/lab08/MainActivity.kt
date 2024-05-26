baa7dd1ab6a11fb25fcce3ac88fce80f1dc3e9e5
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-05-26 11:27:52 +0800

    latest updated

--- app/src/main/java/com/my/muhammadaliftajudin/lab08/MainActivity.kt
+++ app/src/main/java/com/my/muhammadaliftajudin/lab08/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.my.muhammadaliftajudin.lab08
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.my.muhammadaliftajudin.lab08.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +27,35 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        
+        // Date Button
+        var dateButton = binding.dateBtn;
+        var timeButton = binding.timeBtn;
+        var dateTextView = binding.dateTextView;
+        var timeTextView = binding.timeTextView;
+
+        dateButton.setOnClickListener {
+            val c = Calendar.getInstance()
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val month = c.get(Calendar.MONTH)
+            val year = c.get(Calendar.YEAR)
+
+            val myDatePicker = DatePickerDialog(this, android.R.style.ThemeOverlay,
+                DatePickerDialog.OnDateSetListener { DatePicker, Year, Month, Day ->
+                    dateTextView.text = "$Day/${Month + 1}/$Year"}, year, month, day)
+            myDatePicker.show()
+        }
+
+        timeButton.setOnClickListener {
+            val c = Calendar.getInstance()
+            val hour = c.get(Calendar.HOUR)
+            val minutes = c.get(Calendar.MINUTE)
+            
+            val myTimePicker = TimePickerDialog(this, android.R.style.ThemeOverlay,
+                TimePickerDialog.OnTimeSetListener { TimePicker, hourOfDay, minute ->
+                    timeTextView.text = "$hourOfDay:$minute"}, hour, minutes, true)
+            myTimePicker.show()
         }
     }
 }
---
+++ app/src/main/java/com/my/muhammadaliftajudin/lab08/ThanksActivity.kt
@@ -0,0 +1,20 @@
+package com.my.muhammadaliftajudin.lab08
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThanksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_thanks)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
